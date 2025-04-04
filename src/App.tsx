86d62e60592048a51d18a252839c21d02435105b
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-04-03 23:55:45 -0300

    Add image carousel prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Carousel from "./components/Carousel";
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const SLIDE_COUNT = 10;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 function App() {
   return (
     <main>
+      <h3>¡Bienvenido!</h3>
       <h1>CABAÑAS BARILOCHE</h1>
+      <h2>Tu lugar en la Patagonia</h2>
+
+      <Carousel slides={SLIDES} />
     </main>
   );
 }
