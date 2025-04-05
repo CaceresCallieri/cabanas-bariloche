3e7423b75fef4996ad54eda9863a2894d5b7b3fe
Nomad Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-04-05 19:07:47 -0300

    Correctly order images from "Ana" cottage

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import "./App.css";
 import Carousel from "./components/Carousel";
 
 const cottageImagesPaths = [
-  "/cabañas/cabaña-ana/Baño 2.webp",
-  "/cabañas/cabaña-ana/Baño.webp",
-  "/cabañas/cabaña-ana/Cocina.webp",
-  "/cabañas/cabaña-ana/Dormitorio Principal 2.webp",
-  "/cabañas/cabaña-ana/Dormitorio Principal.webp",
-  "/cabañas/cabaña-ana/Dormitorio Secudario 2.webp",
-  "/cabañas/cabaña-ana/Dormitorio Secudario.webp",
-  "/cabañas/cabaña-ana/Dormitorio Secundario 3.webp",
-  "/cabañas/cabaña-ana/Exterior.webp",
-  "/cabañas/cabaña-ana/Sala de Estar 2.webp",
-  "/cabañas/cabaña-ana/Sala de Estar.webp",
+  "/cabañas/cabaña-ana/1.Dormitorio Principal.webp",
+  "/cabañas/cabaña-ana/2.Dormitorio Principal 2.webp",
+  "/cabañas/cabaña-ana/3.Dormitorio Secudario.webp",
+  "/cabañas/cabaña-ana/4.Dormitorio Secudario 2.webp",
+  "/cabañas/cabaña-ana/5.Dormitorio Secundario 3.webp",
+  "/cabañas/cabaña-ana/6.Sala de Estar.webp",
+  "/cabañas/cabaña-ana/7.Sala de Estar 2.webp",
+  "/cabañas/cabaña-ana/8.Cocina.webp",
+  "/cabañas/cabaña-ana/9.Baño.webp",
+  "/cabañas/cabaña-ana/10.Baño 2.webp",
+  "/cabañas/cabaña-ana/11.Exterior.webp",
 ];
 
 function App() {
