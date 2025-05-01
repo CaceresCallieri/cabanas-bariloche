b9bbc6440e8ecd731801acd6547ada908207e75c
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-04-30 23:32:07 -0300

    Animation: Add animation split text animation to subheader

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { useState } from "react";
-
+import { motion } from "motion/react";
 import getCottageImages from "./utils/getCottageImages";
 
 import Carousel from "./components/Carousel/Carousel";
@@ -16,12 +16,25 @@ const COTTAGE_IMAGES = getCottageImages();
 function App() {
   const [selectedCottage, setSelectedCottage] = useState(COTTAGES[0]);
 
+  const text = "Tu lugar en la Patagonia...";
+
   return (
     <>
       <header>
         <h5>¡Bienvenido!</h5>
         <h1>CABAÑAS BARILOCHE</h1>
-        <h3>Tu lugar en la Patagonia...</h3>
+        <h3>
+          {text.split("").map((letter, index) => (
+            <motion.span
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.1, delay: index * 0.05 }}
+              key={index}
+            >
+              {letter}
+            </motion.span>
+          ))}
+        </h3>
       </header>
 
       <main>
