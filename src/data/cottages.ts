06ea5df8cd9a0a784c6de585ed2176c9f4430ba2
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-05-02 20:29:43 -0300

    Data: Update cottage desciptions

--- src/data/cottages.ts
+++ src/data/cottages.ts
@@ -6,10 +6,11 @@ const COTTAGES: COTTAGE[] = [
     code: "ruben",
     location: "Belgrano",
     description: [
-      "Luminoso living con televisor para disfrutar en familia",
+      "Luminoso living para disfrutar en familia",
       "Confort para hasta 5 personas en 2 cómodas habitaciones",
       "Cocina totalmente equipada, lista para tus comidas",
       "Dos baños para mayor comodidad durante tu estadía",
+      "Estacionamiento incluido para tu vehículo",
     ],
   },
   {
@@ -20,6 +21,7 @@ const COTTAGES: COTTAGE[] = [
       "Espacioso living con cocina integrada, ideal para compartir",
       "Diseño de dos plantas que brinda mayor privacidad y confort",
       "Habitación amplia con capacidad para 2 a 3 personas",
+      "Estacionamiento incluido para tu vehículo",
     ],
   },
   {
@@ -27,9 +29,10 @@ const COTTAGES: COTTAGE[] = [
     code: "trinidad",
     location: "Belgrano",
     description: [
-      "Acogedor living con cocina integrada y televisor incluido",
+      "Acogedor living con cocina integrada",
       "Dos habitaciones confortables para hasta 5 huéspedes",
       "Baño completo con bañera para un merecido relax",
+      "Estacionamiento incluido para tu vehículo",
     ],
   },
   {
@@ -37,9 +40,10 @@ const COTTAGES: COTTAGE[] = [
     code: "michelle",
     location: "Belgrano",
     description: [
-      "Ambiente integrado con living, cocina y televisor",
+      "Ambiente integrado con living y cocina",
       "Espacio ideal para 3 personas, cómodo y funcional",
       "Baño con bañera para disfrutar de un buen descanso",
+      "Estacionamiento incluido para tu vehículo",
     ],
   },
   {
@@ -47,10 +51,11 @@ const COTTAGES: COTTAGE[] = [
     code: "anahi",
     location: "Las Victorias",
     description: [
-      "Living acogedor con televisor para momentos de relax",
+      "Living acogedor para momentos de relax",
       "Dos habitaciones amplias con capacidad para 6 personas",
       "Baño completo con bañera para mayor confort",
       "Acceso directo a un patio exterior para disfrutar del aire libre",
+      "Estacionamiento incluido para tu vehículo",
     ],
   },
 ];
