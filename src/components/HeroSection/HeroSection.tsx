299b858dbec3ccbb99e97df0bfebd012091a9f85
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-05-09 00:05:31 -0300

    Animation-HeroSection: Add easing to text splitting animation

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,23 +2,31 @@ import "./HeroSection.css";
 import { motion } from "motion/react";
 
 const HeroSection = () => {
-  const text = "Tu lugar en la Patagonia...";
+  const subHeaderText = "Tu lugar en la Patagonia...";
+
+  // Easing function for delay
+  const easeInQuad = (t: number) => t * t;
 
   return (
     <header>
       <h5>¡Bienvenido!</h5>
       <h1>CABAÑAS BARILOCHE</h1>
       <h3>
-        {text.split("").map((letter, index) => (
-          <motion.span
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.1, delay: index * 0.05 }}
-            key={index}
-          >
-            {letter}
-          </motion.span>
-        ))}
+        {subHeaderText.split("").map((letter, index, array) => {
+          // Calculate the delay using the easing function
+          const delay = easeInQuad(index / array.length) * 1.75;
+
+          return (
+            <motion.span
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.1, delay }}
+              key={index}
+            >
+              {letter}
+            </motion.span>
+          );
+        })}
       </h3>
     </header>
   );
