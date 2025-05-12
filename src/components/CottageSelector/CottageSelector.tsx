eb6750f62165a93807923b09a45328c7fa857f34
Nomad Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-05-12 01:38:26 -0300

    Style-Layout: Add house icon to Cottage Selector dropdown menu

--- src/components/CottageSelector/CottageSelector.tsx
+++ src/components/CottageSelector/CottageSelector.tsx
@@ -1,6 +1,9 @@
 import "./CottageSelector.css";
+
 import React, { Dispatch, SetStateAction } from "react";
 import { Dropdown } from "primereact/dropdown";
+import HouseIcon from "../../assets/house.svg?react";
+
 import { COTTAGE } from "../../types";
 
 interface CottageSelectorProps {
@@ -15,6 +18,7 @@ const CottageSelector: React.FC<CottageSelectorProps> = (props) => {
   return (
     <div className="cottage-selector">
       {/* TODO: Add house/cottage svg/icon/emoji to the left of the dropdown */}
+      <HouseIcon className="house-icon" />
       <Dropdown
         value={selectedCottage}
         onChange={(event) => setSelectedCottage(event.value)}
