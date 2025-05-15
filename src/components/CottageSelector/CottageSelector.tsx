2b4c43aa2b34a8fe177efa01d0bd9ee464f47028
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-05-15 17:11:17 -0300

    Styles: Disable HouseIcon

--- src/components/CottageSelector/CottageSelector.tsx
+++ src/components/CottageSelector/CottageSelector.tsx
@@ -2,7 +2,7 @@ import "./CottageSelector.css";
 
 import React, { Dispatch, SetStateAction } from "react";
 import { Dropdown } from "primereact/dropdown";
-import HouseIcon from "../../assets/house.svg?react";
+// import HouseIcon from "../../assets/house.svg?react";
 
 import { COTTAGE } from "../../types";
 
@@ -17,8 +17,7 @@ const CottageSelector: React.FC<CottageSelectorProps> = (props) => {
 
   return (
     <div className="cottage-selector">
-      {/* TODO: Add house/cottage svg/icon/emoji to the left of the dropdown */}
-      <HouseIcon className="house-icon" />
+      {/* <HouseIcon className="house-icon" /> */}
       <Dropdown
         value={selectedCottage}
         onChange={(event) => setSelectedCottage(event.value)}
