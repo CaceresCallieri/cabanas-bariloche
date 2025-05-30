d3061c497c2dd89b32bd4be30fd6ae328f40216b
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-05-30 00:39:22 -0300

    Feature-Footer: Add link to developer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+const whatsAppLink = "https://wa.me/5493513271238";
+
+const Footer = () => (
+  <footer>
+    <p>© {new Date().getFullYear()} Cabañas Bariloche.</p>
+
+    <div className="developed-by">
+      <p>Desarrollado por</p>{" "}
+      <a href={whatsAppLink} target="_blank">
+        Juan Cruz Caceres
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
