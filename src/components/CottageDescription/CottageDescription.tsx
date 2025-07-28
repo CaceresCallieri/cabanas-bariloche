c4a3dc1a08ec38693d14c1985504c6eeb01b0bd6
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-07-27 23:25:03 -0300

    Style-CottageDescription: Improve html structure; Add margins to the card to avoid clustering to the borders on mobile devices

--- src/components/CottageDescription/CottageDescription.tsx
+++ src/components/CottageDescription/CottageDescription.tsx
@@ -3,17 +3,19 @@ import React from "react";
 import { COTTAGE } from "../../types";
 
 const CottageDescription: React.FC<{ selectedCottage: COTTAGE }> = ({ selectedCottage }) => (
-	<div className="cottage-description glass-effect">
-		<ul>
-			{selectedCottage.description.map((sentence, index) => (
-				<li key={index}>{sentence}</li>
-			))}
-			{selectedCottage.location === "Belgrano" && (
-				<li key={selectedCottage.code}>
-					Ubicación céntrica, ideal para explorar la ciudad.
-				</li>
-			)}
-		</ul>
+	<div className="cottage-description">
+		<div className="cottage-description-card glass-effect">
+			<ul>
+				{selectedCottage.description.map((sentence, index) => (
+					<li key={index}>{sentence}</li>
+				))}
+				{selectedCottage.location === "Belgrano" && (
+					<li key={selectedCottage.code}>
+						Ubicación céntrica, ideal para explorar la ciudad.
+					</li>
+				)}
+			</ul>
+		</div>
 	</div>
 );
 
